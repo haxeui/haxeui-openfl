b2e8fb479dc9c68cb64282f9f29ad243a2a053d9
Pablo Martin <theawakening@gmail.com>
2017-07-24 03:30:37 +0200

    Default setting with the system language.

--- haxe/ui/backend/PlatformBase.hx
+++ haxe/ui/backend/PlatformBase.hx
@@ -1,6 +1,9 @@
 package haxe.ui.backend;
 
+import flash.system.Capabilities;
 class PlatformBase {
+    private var _systemLocale:String = Capabilities.language;
+
     public function getMetric(id:String):Float {
         return 0;
     }
