bea7175149fcb5a5b00195b7b492ec6b668deafd
Ian Harrigan <ianharrigan@hotmail.com>
2017-07-28 12:53:59 +0200

    use ENTER_FRAME

--- haxe/ui/backend/CallLaterBase.hx
+++ haxe/ui/backend/CallLaterBase.hx
@@ -1,16 +1,18 @@
 package haxe.ui.backend;
 
-import haxe.ui.util.Timer;
+import openfl.Lib;
+import openfl.events.Event;
 
 class CallLaterBase {
-    private var _timer:Timer = null;
+    private var _fn:Void->Void;
+    
     public function new(fn:Void->Void) {
-        _timer = new Timer(0, function() {
-            if (_timer != null) {
-                _timer.stop();
-                _timer = null;
-                fn();
-            }
-        });
+        _fn = fn;
+        Lib.current.stage.addEventListener(Event.ENTER_FRAME, onEnterFrame);
+    }
+    
+    private function onEnterFrame(e) {
+        Lib.current.stage.removeEventListener(Event.ENTER_FRAME, onEnterFrame);
+        _fn();
     }
 }
