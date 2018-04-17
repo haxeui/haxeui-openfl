96c97b0e53395de5d0f21964224cd8b0491812ff
Pablo Martin <theawakening@gmail.com>
2018-04-18 00:09:32 +0200

    Callback function from the Timer class is executed inside of the ENTER_FRAME event.

--- haxe/ui/backend/CallLaterBase.hx
+++ haxe/ui/backend/CallLaterBase.hx
@@ -1,18 +1,7 @@
 package haxe.ui.backend;
 
-import openfl.Lib;
-import openfl.events.Event;
-
 class CallLaterBase {
-    private var _fn:Void->Void;
-    
     public function new(fn:Void->Void) {
-        _fn = fn;
-        Lib.current.stage.addEventListener(Event.ENTER_FRAME, onEnterFrame, false, 10000);
-    }
-    
-    private function onEnterFrame(e) {
-        Lib.current.stage.removeEventListener(Event.ENTER_FRAME, onEnterFrame);
-        _fn();
+        haxe.ui.util.Timer.delay(fn, 0);
     }
 }
