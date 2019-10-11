1bfaeac16ce13ce96508e96f38aea9e2f74108d1
Ian Harrigan <ianharrigan@hotmail.com>
2019-10-11 18:18:49 +0200

    style improvement

--- haxe/ui/backend/openfl/OpenFLStyleHelper.hx
+++ haxe/ui/backend/openfl/OpenFLStyleHelper.hx
@@ -150,7 +150,7 @@ class OpenFLStyleHelper {
         if (borderRadius == 0) {
             graphics.drawRect(rc.left, rc.top, rc.width, rc.height);
         } else {
-            graphics.drawRoundRect(rc.left, rc.top, rc.width, rc.height, borderRadius + 2, borderRadius + 2);
+            graphics.drawRoundRect(rc.left, rc.top, rc.width, rc.height, borderRadius, borderRadius);
         }
 
         graphics.endFill();
