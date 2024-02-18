b0a2b76c2164138e99cf5d1ce4d2f1e88c3260a2
Ian Harrigan <ianharrigan@hotmail.com>
2024-02-18 15:43:39 +0100

    additional params in screen.removeComponent

--- haxe/ui/backend/ScreenImpl.hx
+++ haxe/ui/backend/ScreenImpl.hx
@@ -63,7 +63,7 @@ class ScreenImpl extends ScreenBase {
         return component;
     }
 
-    public override function removeComponent(component:Component, dispose:Bool = true):Component {
+    public override function removeComponent(component:Component, dispose:Bool = true, invalidate:Bool = true):Component {
         rootComponents.remove(component);
         container.removeChild(component);
         return component;
